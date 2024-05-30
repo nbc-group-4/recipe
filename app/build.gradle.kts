34e386c3a92d1a6bebc2016b1973164630cdc7b3
배기정 <ligerbae@gmail.com>
2024-05-30 14:47:56 +0900

    PR 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
         buildConfigField("String", "RECIPE_API_BASE", properties.getProperty("RECIPE_API_BASE"))
         buildConfigField("String", "SPECIALTY_API_KEY", properties.getProperty("SPECIALTY_API_KEY"))
         buildConfigField("String", "RECIPE_API_KEY", properties.getProperty("RECIPE_API_KEY"))
+        buildConfigField("String", "KAKAO_MAP_KEY", properties.getProperty("KAKAO_MAP_KEY"))
+        buildConfigField("String", "SEARCH_API_BASE", properties.getProperty("SEARCH_API_BASE"))
+        buildConfigField("String", "SEARCH_REST_API_KEY", properties.getProperty("SEARCH_REST_API_KEY"))
     }
 
     buildTypes {
