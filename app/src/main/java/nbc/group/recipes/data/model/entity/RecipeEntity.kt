49d5a2f2862e733aba4937c087c6444179f27042
YEJIN <jindorianhuskey@gmail.com>
2024-05-31 16:10:40 +0900

    feat: RecipeEntity 수정 및 정의

--- app/src/main/java/nbc/group/recipes/data/model/entity/RecipeEntity.kt
+++ app/src/main/java/nbc/group/recipes/data/model/entity/RecipeEntity.kt
@@ -1,9 +1,14 @@
 package nbc.group.recipes.data.model.entity
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 @Entity
 data class RecipeEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long
+    val recipeImg: String,
+    val recipeName: String,
+    val id: Int,
+    val explain: String,
+    val step: String,
+    val ingredient: String,
+    val difficulty: String,
 )
