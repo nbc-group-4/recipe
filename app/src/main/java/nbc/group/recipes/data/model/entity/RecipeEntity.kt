b23c9d95658e5671f74f8f69ea1789855ce289e5
YEJIN <jindorianhuskey@gmail.com>
2024-06-03 10:05:07 +0900

    feat: RecipeEntity 수정 및 xml 작업 관련 파일 생성 및 수정

--- app/src/main/java/nbc/group/recipes/data/model/entity/RecipeEntity.kt
+++ app/src/main/java/nbc/group/recipes/data/model/entity/RecipeEntity.kt
@@ -1,14 +1,16 @@
 package nbc.group.recipes.data.model.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class RecipeEntity(
     val recipeImg: String,
     val recipeName: String,
-    val id: Int,
+    @PrimaryKey val id: Int,
     val explain: String,
     val step: String,
     val ingredient: String,
     val difficulty: String,
+    val time: String,
 )
--- app/src/main/java/nbc/group/recipes/presentation/fragment/RecipeFragment.kt
+++ app/src/main/java/nbc/group/recipes/presentation/fragment/RecipeFragment.kt
@@ -5,31 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import nbc.group.recipes.R
+import nbc.group.recipes.databinding.FragmentRecipeBinding
+import nbc.group.recipes.viewmodel.MainViewModel
+import nbc.group.recipes.viewmodel.MainViewModel_Factory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RecipeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecipeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentRecipeBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val binding: FragmentRecipeBinding
+        get() = _binding!!
+
+    private val viewModel by viewModels<MainViewModel> {
+        MainViewModel()
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
