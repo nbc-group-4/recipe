0285c90255ef759235c37a17fced38d96710a7fc
treeralph <treeralph@gmail.com>
2024-06-03 19:42:18 +0900

    feat: SignIn, SignUp xml 작업 및 테스트 코드 작성

--- app/src/main/java/nbc/group/recipes/presentation/fragment/MypageFragment.kt
+++ app/src/main/java/nbc/group/recipes/presentation/fragment/MypageFragment.kt
@@ -1,21 +1,31 @@
 package nbc.group.recipes.presentation.fragment
 
+import android.Manifest
 import android.app.ProgressDialog
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import nbc.group.recipes.GlideApp
 import nbc.group.recipes.R
+import nbc.group.recipes.StorageGlideModule
+import nbc.group.recipes.data.model.dto.Recipe
 import nbc.group.recipes.data.network.FirebaseResult
 import nbc.group.recipes.databinding.FragmentMypageBinding
 import nbc.group.recipes.viewmodel.MainViewModel
+import java.io.File
 
 @AndroidEntryPoint
 class MypageFragment : Fragment() {
@@ -28,6 +38,25 @@ class MypageFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel: MainViewModel by activityViewModels()
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if(isGranted) {
+                Log.e(TAG, "Permission Granted")
+            }
+        }
+
+    private val pickMedia = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri ->
+        if(uri != null) {
+            viewLifecycleOwner.lifecycleScope.launch {
+                val inputStream = requireActivity().contentResolver.openInputStream(uri)
+                viewModel.putImage(inputStream!!)
+                binding.testImageView.setImageURI(uri)
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,6 +67,34 @@ class MypageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requestPermissionLauncher.launch(
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        binding.testImageView2.setOnClickListener {
+            GlideApp.with(this)
+                .load(Firebase.storage.reference.child("userProfile/jun/profile.jpg"))
+                .into(binding.testImageView2)
+        }
+
+        with(binding) {
+            testButton1.setOnClickListener {
+                viewModel.putRecipe(
+                    Recipe()
+                )
+            }
+
+            testButton2.setOnClickListener {
+                viewModel.getRecipe()
+            }
+
+            testImageView.setOnClickListener {
+                pickMedia.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            }
+        }
 
         viewModel.currentUser?.let {
             binding.tv.text = "${it.email}"
@@ -53,6 +110,26 @@ class MypageFragment : Fragment() {
                         }
                         is FirebaseResult.Failure -> {
                             Log.e(SignInFragment.TAG, "onViewCreated: Failure", )
+                            binding.tv.text = ""
+                        }
+                        is FirebaseResult.Loading -> {
+
+                        }
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.recipes.collect { nullable ->
+                if(nullable == null) binding.tv.text = "There are no recipes"
+                nullable?.let { nonNull ->
+                    when(nonNull) {
+                        is FirebaseResult.Success -> {
+                            binding.tv.text = nonNull.result.toString()
+                        }
+                        is FirebaseResult.Failure -> {
+                            Log.e(SignInFragment.TAG, "onViewCreated: Failure: ${nonNull.exception}", )
                             binding.tv.text = ""
                         }
                         is FirebaseResult.Loading -> {
