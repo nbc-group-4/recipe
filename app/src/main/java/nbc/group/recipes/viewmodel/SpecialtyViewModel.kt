c8968a40da42777511c9155a28d04028d486e42e
배기정 <ligerbae@gmail.com>
2024-06-07 12:37:47 +0900

    fix: 뷰모델

---
+++ app/src/main/java/nbc/group/recipes/viewmodel/SpecialtyViewModel.kt
@@ -0,0 +1,43 @@
+package nbc.group.recipes.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import nbc.group.recipes.KindItem
+import nbc.group.recipes.data.model.dto.Item
+import nbc.group.recipes.data.repository.RecipeSpecialtyRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class SpecialtyViewModel @Inject constructor(
+    private val repository: RecipeSpecialtyRepository,
+    private val sharedViewModel: SpecialtyViewModel?
+) : ViewModel() {
+
+    private val _selectedKindItem = MutableStateFlow<KindItem?>(null)
+    val selectedKindItem = _selectedKindItem.asStateFlow()
+
+    private val _searchResult = MutableStateFlow<List<Item>>(emptyList())
+    val searchResult = _searchResult.asStateFlow()
+
+    fun setSelectedKindItem(kindItem: KindItem) {
+        _selectedKindItem.value = kindItem
+    }
+
+    private fun setSearchResult(result: List<Item>) {
+        _searchResult.value = result
+    }
+
+    fun searchItem(searchQuery: String) {
+        viewModelScope.launch {
+            val specialtyResponse = repository.getSpecialty("")
+            val filteredItems = specialtyResponse.body.items.item.filter { item ->
+                item.cntntsSj?.contains(searchQuery, ignoreCase = true) == true
+            }
+            sharedViewModel?.setSearchResult(filteredItems)
+        }
+    }
+}
