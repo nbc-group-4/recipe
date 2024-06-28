9b55772cb2c86b240a9cfef91e25bcffa51b132a
배기정 <ligerbae@gmail.com>
2024-06-28 21:04:55 +0900

    fix : 파일 업데이트

--- app/src/main/java/nbc/group/recipes/viewmodel/MypageSharedViewModel.kt
+++ app/src/main/java/nbc/group/recipes/viewmodel/MypageSharedViewModel.kt
@@ -6,14 +6,11 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
-@HiltViewModel
-class MypageSharedViewModel @Inject constructor() : ViewModel() {
-
-    // 클릭한 작성 레시피
-    private val _selectedRecipe = MutableStateFlow<String?>(null)
-    val selectedRecipe: StateFlow<String?> = _selectedRecipe
-
-    fun selectRecipe(recipeId: String) {
-        _selectedRecipe.value = recipeId
-    }
-}
+//@HiltViewModel
+//class MypageSharedViewModel @Inject constructor() : ViewModel() {
+//    private val _selectedRecipe = MutableStateFlow<String?>(null)
+//    val selectedRecipe: StateFlow<String?> = _selectedRecipe
+//    fun selectRecipe(recipeId: String) {
+//        _selectedRecipe.value = recipeId
+//    }
+//}
