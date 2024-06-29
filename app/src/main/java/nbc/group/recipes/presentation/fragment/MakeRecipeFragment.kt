4c9db12c2367ab1953579caed65f1d67a97aa915
wndnjs00 <juwonjeon852@gmail.com>
2024-06-29 11:41:50 +0900

    feat: MakeRecipeFragment 레이아웃, 코드 수정

--- app/src/main/java/nbc/group/recipes/presentation/fragment/MakeRecipeFragment.kt
+++ app/src/main/java/nbc/group/recipes/presentation/fragment/MakeRecipeFragment.kt
@@ -1,8 +1,11 @@
 package nbc.group.recipes.presentation.fragment
 
 import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -87,6 +90,11 @@ class MakeRecipeFragment : Fragment() {
                 LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
             rvImages.addItemDecoration(itemDecoration)
             rvImages.adapter = adapter
+
+            etRecipeName.addTextChangedListener(textWatcher)
+            etRecipeDescription.addTextChangedListener(textWatcher)
+            etIngredient.addTextChangedListener(textWatcher)
+            etCookingProcess.addTextChangedListener(textWatcher)
 
             setupSpinner(spinnerCookingTime)
         }
@@ -194,4 +202,33 @@ class MakeRecipeFragment : Fragment() {
             }
         }
     }
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            updateButton()
+        }
+        override fun afterTextChanged(s: Editable?) {
+        }
+    }
+
+    private fun updateButton() {
+        with(binding) {
+            val isRecipeName = etRecipeName.text.toString().isNotEmpty()
+            val isRecipeDescription = etRecipeDescription.text.toString().isNotEmpty()
+            val isIngredient = etIngredient.text.toString().isNotEmpty()
+            val isCookingProcess = etCookingProcess.text.toString().isNotEmpty()
+
+            val allFieldsValid = isRecipeName && isRecipeDescription && isIngredient && isCookingProcess
+
+            if(allFieldsValid){
+                btMakeRecipe.isEnabled = true
+                btMakeRecipe.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.green1))
+            }else{
+                btMakeRecipe.isEnabled = false
+                btMakeRecipe.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.unclick_btn))
+            }
+        }
+    }
+
 }
