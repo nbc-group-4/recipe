16c02a4a3a6e8d72de146f2faa2fe4dc27afd1a0
YEJIN <jindorianhuskey@gmail.com>
2024-06-29 11:33:43 +0900

    fix: MakeRecipeFragment 변수명 수정

--- app/src/main/java/nbc/group/recipes/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/nbc/group/recipes/presentation/fragment/HomeFragment.kt
@@ -139,10 +139,10 @@ class HomeFragment : Fragment() {
     }
 
     private fun autoBannerScroll() = object  : Runnable{
-            override fun run() {
-                binding.homeBanner.currentItem = (binding.homeBanner.currentItem + 1) % (binding.homeBanner.adapter?.itemCount ?: 1)
-                handler.postDelayed(this, 2000)
-            }
+        override fun run() {
+            binding.homeBanner.currentItem = (binding.homeBanner.currentItem + 1) % (binding.homeBanner.adapter?.itemCount ?: 1)
+            handler.postDelayed(this, 2000)
+        }
     }
 
 }
